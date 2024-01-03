ef1dade87c7ab9498b10363137fd1694c65670f8
AlissonSteffens <alisson.steffens@hotmail.com>
2024-01-02 22:00:00 -0300

    :art: fix footer

--- footer.php
+++ footer.php
@@ -5,15 +5,13 @@ $current_year = date('Y');
 <footer class="footer">
 
   <div class="copy">
-    &copy; <?php echo $current_year ?> - SBBD. Todos os direitos reservados.
     <span class="icon has-text-info">
       <i class="fas fa-code"></i>
     </span> with
     <span class="icon has-text-danger">
       <i class="far fa-heart"></i>
-    </span>
-    by
-    <a href="https://alissonsteffens.com" target="_blank">Alisson</a>.
+    </span> by <a href="https://alissonsteffens.com" target="_blank">Alisson</a> &copy; <?php echo $current_year ?>
+
   </div>
 </footer>
 
